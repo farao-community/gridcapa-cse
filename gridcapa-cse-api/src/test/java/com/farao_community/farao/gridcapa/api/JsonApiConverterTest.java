7e4ad694d61b963d81effdfe18862ac135261f44
AMIRA KAHYA <amira.kahya@rte-france.com>
2021-10-12 16:46:21 +0200

    add author and copyright to JsonApiConverterTest class
    Signed-off-by: AMIRA KAHYA <amira.kahya@rte-france.com>

--- gridcapa-cse-api/src/test/java/com/farao_community/farao/gridcapa/api/JsonApiConverterTest.java
+++ gridcapa-cse-api/src/test/java/com/farao_community/farao/gridcapa/api/JsonApiConverterTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.farao_community.farao.gridcapa.api;
 
 import com.farao_community.farao.gridcapa_cse.api.JsonApiConverter;
@@ -13,6 +19,9 @@ import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * @author Amira Kahya {@literal <amira.kahya at rte-france.com>}
+ */
 class JsonApiConverterTest {
     @Test
     void checkCseRequestJsonConversion() throws IOException {
