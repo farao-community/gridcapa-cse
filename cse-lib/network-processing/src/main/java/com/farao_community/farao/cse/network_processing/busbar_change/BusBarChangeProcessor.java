a8ae327306c7c853a0e826f62e4bd863f6736d15
Peter <47452063+pet-mit@users.noreply.github.com>
2022-01-19 15:04:35 +0100

    Busbar change pre-processor (#11)
    * added json importer for cse hvdc conf, and methods to create hvdc lines and revert creation
    
    * handle tielines
    
    * better handle tielines + PSTs
    
    * Do not copy gens and loads. Allow 3-node cases.
    
    * update farao parent to release version
    
    Co-authored-by: EDWARDS Philippe <philippe.edwards@rte-france.com>
    Co-authored-by: Peter Mitri <peter.mitri@rte-france.com>

---
+++ cse-lib/network-processing/src/main/java/com/farao_community/farao/cse/network_processing/busbar_change/BusBarChangeProcessor.java
@@ -0,0 +1,198 @@
+/*
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+package com.farao_community.farao.cse.network_processing.busbar_change;
+
+import com.farao_community.farao.commons.FaraoException;
+import com.farao_community.farao.data.crac_creation.creator.cse.CseCrac;
+import com.farao_community.farao.data.crac_creation.creator.cse.CseCracCreator;
+import com.farao_community.farao.data.crac_creation.creator.cse.CseCracImporter;
+import com.farao_community.farao.data.crac_creation.creator.cse.parameters.BusBarChangeSwitches;
+import com.farao_community.farao.data.crac_creation.creator.cse.parameters.SwitchPairId;
+import com.farao_community.farao.data.crac_creation.creator.cse.xsd.TCRACSeries;
+import com.farao_community.farao.data.crac_creation.creator.cse.xsd.TRemedialAction;
+import com.farao_community.farao.data.crac_creation.creator.cse.xsd.TRemedialActions;
+import com.farao_community.farao.data.crac_creation.util.ucte.UcteBusHelper;
+import com.farao_community.farao.data.crac_creation.util.ucte.UcteNetworkAnalyzer;
+import com.farao_community.farao.data.crac_creation.util.ucte.UcteNetworkAnalyzerProperties;
+import com.powsybl.iidm.network.Bus;
+import com.powsybl.iidm.network.Network;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static com.farao_community.farao.cse.network_processing.busbar_change.NetworkHelper.SwitchPairToCreate;
+
+/**
+ * This class processes a CSE crac file in order to create, in the network, switches that can be used to apply
+ * BusBar remedial actions
+ *
+ * @author Peter Mitri {@literal <peter.mitri@rte-france.com>}
+ */
+public final class BusBarChangeProcessor {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BusBarChangeProcessor.class);
+
+    private BusBarChangeProcessor() {
+        // utility class
+    }
+
+    public static Set<BusBarChangeSwitches> process(Network network, InputStream cracInputStream) {
+        CseCracImporter importer = new CseCracImporter();
+        CseCrac cseCrac = importer.importNativeCrac(cracInputStream);
+
+        UcteNetworkAnalyzer ucteNetworkAnalyzer = new UcteNetworkAnalyzer(network, new UcteNetworkAnalyzerProperties(UcteNetworkAnalyzerProperties.BusIdMatchPolicy.COMPLETE_WITH_WILDCARDS));
+        NetworkModifier networkModifier = new NetworkModifier(network);
+
+        Map<String, Set<SwitchPairToCreate>> switchesToCreatePerRa = new HashMap<>();
+        SortedSet<BusToCreate> busesToCreate = new TreeSet<>();
+        Map<String, Map<String, String>> createdSwitches = new HashMap<>();
+
+        TCRACSeries tcracSeries = CseCracCreator.getCracSeries(cseCrac.getCracDocument());
+        List<TRemedialActions> tRemedialActionsList = tcracSeries.getRemedialActions();
+
+        for (TRemedialActions tRemedialActions : tRemedialActionsList) {
+            if (tRemedialActions != null) {
+                tRemedialActions.getRemedialAction().stream()
+                    .filter(tRemedialAction -> tRemedialAction.getBusBar() != null)
+                    .forEach(tRemedialAction -> {
+                        try {
+                            computeBusesAndSwitchesToCreate(tRemedialAction, ucteNetworkAnalyzer, busesToCreate, switchesToCreatePerRa);
+                        } catch (FaraoException e) {
+                            LOGGER.warn("RA {} has been skipped: {}", tRemedialAction.getName().getV(), e.getMessage());
+                        }
+                    });
+            }
+        }
+        createBuses(busesToCreate, networkModifier);
+        createSwitches(switchesToCreatePerRa, createdSwitches, networkModifier);
+        networkModifier.commitAllChanges();
+        return computeBusBarChangeSwitches(switchesToCreatePerRa, createdSwitches);
+    }
+
+    /**
+     * For every BusBar TRemedialAction, this method detects and stores info about elements that should be created:
+     * - buses, if initial or final node does not exist in the network (info stored in busesToCreate)
+     * - switches, to be able to move the line from the initial to the final node (info stored in switchesToCreatePerRa)
+     */
+    private static void computeBusesAndSwitchesToCreate(TRemedialAction tRemedialAction, UcteNetworkAnalyzer ucteNetworkAnalyzer, SortedSet<BusToCreate> busesToCreate, Map<String, Set<SwitchPairToCreate>> switchesToCreatePerRa) {
+        Optional<BusToCreate> raBusToCreate = Optional.empty();
+        Set<SwitchPairToCreate> raSwitchesToCreate = new HashSet<>();
+
+        // Get initial and final nodes
+        UcteBusHelper initialNodeHelper = new UcteBusHelper(tRemedialAction.getBusBar().getInitialNode().getV(), ucteNetworkAnalyzer);
+        UcteBusHelper finalNodeHelper = new UcteBusHelper(tRemedialAction.getBusBar().getFinalNode().getV(), ucteNetworkAnalyzer);
+
+        String initialNodeId;
+        String finalNodeId;
+        if (initialNodeHelper.isValid() && finalNodeHelper.isValid()) {
+            initialNodeId = initialNodeHelper.getIdInNetwork();
+            finalNodeId = finalNodeHelper.getIdInNetwork();
+        } else if (initialNodeHelper.isValid() && !finalNodeHelper.isValid()) {
+            initialNodeId = initialNodeHelper.getIdInNetwork();
+            finalNodeId = tRemedialAction.getBusBar().getFinalNode().getV();
+            raBusToCreate = Optional.of(new BusToCreate(finalNodeId, ((Bus) ucteNetworkAnalyzer.getNetwork().getIdentifiable(initialNodeId)).getVoltageLevel().getId()));
+        } else if (!initialNodeHelper.isValid() && finalNodeHelper.isValid()) {
+            finalNodeId = finalNodeHelper.getIdInNetwork();
+            initialNodeId = tRemedialAction.getBusBar().getInitialNode().getV();
+            raBusToCreate = Optional.of(new BusToCreate(initialNodeId, ((Bus) ucteNetworkAnalyzer.getNetwork().getIdentifiable(finalNodeId)).getVoltageLevel().getId()));
+        } else {
+            throw new FaraoException(String.format("Remedial action's initial and final nodes are not valid: %s", initialNodeHelper.getInvalidReason()));
+        }
+
+        // Store all switches to create
+        tRemedialAction.getBusBar().getBranch().forEach(tBranch -> {
+            String linId = NetworkHelper.getLineIdInNetwork(tBranch.getFromNode().getV(), tBranch.getToNode().getV(), String.valueOf(tBranch.getOrder().getV()), initialNodeId, finalNodeId, ucteNetworkAnalyzer);
+            raSwitchesToCreate.add(new SwitchPairToCreate(linId, initialNodeId, finalNodeId));
+        });
+
+        // If everything is OK with the RA, store what should be created
+        raBusToCreate.ifPresent(busesToCreate::add);
+        switchesToCreatePerRa.put(tRemedialAction.getName().getV(), raSwitchesToCreate);
+    }
+
+    /**
+     * Creates buses needed in busesToCreate
+     */
+    private static void createBuses(SortedSet<BusToCreate> busesToCreate, NetworkModifier networkModifier) {
+        busesToCreate.forEach(busToCreate -> networkModifier.createBus(busToCreate.busId, busToCreate.voltageLevelId));
+    }
+
+    /**
+     * Creates switches needed in switchesToCreatePerRa
+     * Stores info about created switches in createdSwitches, per connected node (initial/final)
+     */
+    private static void createSwitches(Map<String, Set<SwitchPairToCreate>> switchesToCreatePerRa, Map<String, Map<String, String>> createdSwitches, NetworkModifier networkModifier) {
+        Set<SwitchPairToCreate> uniqueSwitchPairsToCreate = switchesToCreatePerRa.values().stream().flatMap(Set::stream).collect(Collectors.toSet());
+        uniqueSwitchPairsToCreate.stream().sorted().forEach(switchPairToCreate -> createdSwitches.put(switchPairToCreate.uniqueId, NetworkHelper.createSwitchPair(switchPairToCreate, networkModifier)));
+    }
+
+    /**
+     * Maps info in createdSwitches to needed switches for RAs, and creates a BusBarChangeSwitches set
+     */
+    private static Set<BusBarChangeSwitches> computeBusBarChangeSwitches(Map<String, Set<SwitchPairToCreate>> switchesToCreatePerRa, Map<String, Map<String, String>> createdSwitches) {
+        Set<BusBarChangeSwitches> busBarChangeSwitches = new HashSet<>();
+
+        // Loop through the RAs and add switch pairs IDs to the set
+        switchesToCreatePerRa.forEach((raId, switchPairsToCreateForRa) -> {
+            // Loop through the branches of RA and fetch switch pair IDs
+            Set<SwitchPairId> switchPairs = switchPairsToCreateForRa.stream()
+                .map(switchPairToCreate -> getCreatedSwitchPairId(switchPairToCreate, createdSwitches))
+                .collect(Collectors.toSet());
+            busBarChangeSwitches.add(new BusBarChangeSwitches(raId, switchPairs));
+        });
+        return busBarChangeSwitches;
+    }
+
+    /**
+     * Fetches the IDs of the created switches to open & close, for a given SwitchPairToCreate
+     */
+    private static SwitchPairId getCreatedSwitchPairId(SwitchPairToCreate switchPairToCreate, Map<String, Map<String, String>> createdSwitches) {
+        String switchPairToCreateId = switchPairToCreate.uniqueId();
+        // OPEN switch between branch and initial node, CLOSE switch between branch and final node
+        return new SwitchPairId(
+            createdSwitches.get(switchPairToCreateId).get(switchPairToCreate.initialNodeId),
+            createdSwitches.get(switchPairToCreateId).get(switchPairToCreate.finalNodeId)
+        );
+    }
+
+    /**
+     * Contains needed info to create a bus
+     */
+    private static class BusToCreate implements Comparable<BusToCreate> {
+        String busId; // ID of the bus to create
+        String voltageLevelId; // ID of the voltage level to create the bus on
+
+        BusToCreate(String busId, String voltageLevelId) {
+            this.busId = busId;
+            this.voltageLevelId = voltageLevelId;
+        }
+
+        @Override
+        public int hashCode() {
+            return busId.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+            return busId.equals(((BusToCreate) obj).busId);
+        }
+
+        @Override
+        public int compareTo(BusToCreate other) {
+            return this.busId.compareTo(other.busId);
+        }
+    }
+
+}
---
+++ cse-lib/network-processing/src/main/java/com/farao_community/farao/cse/network_processing/busbar_change/NetworkModifier.java
@@ -0,0 +1,337 @@
+/*
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+package com.farao_community.farao.cse.network_processing.busbar_change;
+
+import com.farao_community.farao.commons.FaraoException;
+import com.powsybl.commons.PowsyblException;
+import com.powsybl.iidm.network.*;
+import com.powsybl.ucte.converter.util.UcteConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * This class exposes useful methods to modify a network
+ *
+ * @author Peter Mitri {@literal <peter.mitri@rte-france.com>}
+ */
+public class NetworkModifier {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NetworkModifier.class);
+
+    private Network network;
+    private Set<Connectable<?>> connectablesToRemove;
+
+    public NetworkModifier(Network network) {
+        this.network = network;
+        this.connectablesToRemove = new HashSet<>();
+    }
+
+    public Network getNetwork() {
+        return network;
+    }
+
+    /**
+     * Create a bus with a given ID, on a given voltage level
+     * Use a given reference bus to copy generators & loads
+     */
+    public Bus createBus(String newBusId, String voltageLevelId) {
+        try {
+            VoltageLevel voltageLevel = network.getVoltageLevel(voltageLevelId);
+            Bus newBus = voltageLevel.getBusBreakerView().newBus()
+                .setId(newBusId)
+                .setFictitious(false)
+                .setEnsureIdUnicity(true)
+                .add();
+            findAndSetGeographicalName(newBus, voltageLevel);
+            LOGGER.debug("New bus '{}' has been created", newBus.getId());
+            return newBus;
+        } catch (PowsyblException e) {
+            throw new FaraoException(String.format("Could not create bus %s: %s", newBusId, e.getMessage()));
+        }
+    }
+
+    /**
+     * Create a switch on a given voltage level between two given buses
+     * Set its current limit to a given value & its open/close status to a given status
+     */
+    public Switch createSwitch(VoltageLevel voltageLevel, String bus1Id, String bus2Id, Double currentLimit, boolean open) {
+        try {
+            String switchId = String.format("%s %s 1", bus2Id, bus1Id);
+            Switch newSwitch = voltageLevel.getBusBreakerView().newSwitch()
+                .setId(switchId)
+                .setBus1(bus1Id)
+                .setBus2(bus2Id)
+                .setOpen(open)
+                .setFictitious(false)
+                .setEnsureIdUnicity(true)
+                .add();
+            if (currentLimit != null) {
+                newSwitch.setProperty(UcteConstants.CURRENT_LIMIT_PROPERTY_KEY, String.valueOf((int) currentLimit.doubleValue()));
+            }
+            LOGGER.debug("New switch '{}' has been created", newSwitch.getId());
+            return newSwitch;
+        } catch (PowsyblException e) {
+            throw new FaraoException(String.format("Could not create switch between %s and %s: %s", bus1Id, bus2Id, e.getMessage()));
+        }
+    }
+
+    /**
+     * Move a branch's given side to a given bus
+     * (The method actually copies the branch to a new one then deletes it)
+     *
+     * @param branch the branch to move
+     * @param side   the side of the branch to move
+     * @param bus    the new bus to connect to the side
+     */
+    public void moveBranch(Branch<?> branch, Branch.Side side, Bus bus) {
+        try {
+            if (branch instanceof TwoWindingsTransformer) {
+                moveTwoWindingsTransformer((TwoWindingsTransformer) branch, side, bus);
+            } else if (branch instanceof TieLine) {
+                moveTieLine((TieLine) branch, side, bus);
+            } else if (branch instanceof Line) {
+                moveLine((Line) branch, side, bus);
+            } else {
+                throw new FaraoException(String.format("Cannot move %s of type %s", branch.getId(), branch.getClass()));
+            }
+        } catch (PowsyblException e) {
+            throw new FaraoException(String.format("Could not move line %s: %s", branch.getId(), e.getMessage()));
+        }
+    }
+
+    private static String generateUcteId(String node1, String node2, String suffix) {
+        return String.format("%1$8s %2$8s %3$s", node1, node2, suffix);
+    }
+
+    private static void findAndSetGeographicalName(Bus bus, VoltageLevel voltageLevel) {
+        for (Bus otherBus : voltageLevel.getBusBreakerView().getBuses()) {
+            if (otherBus.hasProperty(UcteConstants.GEOGRAPHICAL_NAME_PROPERTY_KEY)) {
+                bus.setProperty(UcteConstants.GEOGRAPHICAL_NAME_PROPERTY_KEY, otherBus.getProperty(UcteConstants.GEOGRAPHICAL_NAME_PROPERTY_KEY));
+                break;
+            }
+        }
+    }
+
+    private static String getOrderCode(Branch<?> branch) {
+        if (branch.hasProperty(UcteConstants.ORDER_CODE)) {
+            return branch.getProperty(UcteConstants.ORDER_CODE);
+        } else {
+            return branch.getId().substring(branch.getId().length() - 1);
+        }
+    }
+
+    private void moveLine(Line line, Branch.Side side, Bus bus) {
+        String newLineId = replaceSimpleBranchNode(line, side, bus.getId());
+        LineAdder adder = initializeLineAdderToMove(line, newLineId);
+        setBranchAdderProperties(adder, line, side, bus);
+        Line newLine = adder.add();
+        copyCurrentLimits(line, newLine);
+        copyProperties(line, newLine);
+        LOGGER.debug("Line '{}' has been removed, new TieLine '{}' has been created", line.getId(), newLine.getId());
+        connectablesToRemove.add(line);
+    }
+
+    private void moveTieLine(TieLine tieLine, Branch.Side side, Bus bus) {
+        String newLineId = replaceTieLineNode(tieLine, side, bus.getId());
+        TieLineAdder adder = initializeTieLineAdderToMove(tieLine, newLineId, side, bus);
+        setBranchAdderProperties(adder, tieLine, side, bus);
+        TieLine newTieLine = adder.add();
+        copyCurrentLimits(tieLine, newTieLine);
+        copyProperties(tieLine, newTieLine);
+        LOGGER.debug("TieLine '{}' has been removed, new TieLine '{}' has been created", tieLine.getId(), newTieLine.getId());
+        connectablesToRemove.add(tieLine);
+    }
+
+    private void moveTwoWindingsTransformer(TwoWindingsTransformer transformer, Branch.Side side, Bus bus) {
+        String newId = replaceSimpleBranchNode(transformer, side, bus.getId());
+        TwoWindingsTransformerAdder adder = initializeTwoWindingsTransformerAdderToMove(transformer, newId);
+        setBranchAdderProperties(adder, transformer, side, bus);
+        TwoWindingsTransformer newTransformer = adder.add();
+        copyCurrentLimits(transformer, newTransformer);
+        copyProperties(transformer, newTransformer);
+        copyTapChanger(transformer, newTransformer);
+        LOGGER.debug("TwoWindingsTransformer '{}' has been removed, new transformer '{}' has been created", transformer.getId(), newTransformer.getId());
+        connectablesToRemove.add(transformer);
+    }
+
+    private static void copyTapChanger(TwoWindingsTransformer transformerFrom, TwoWindingsTransformer transformerTo) {
+        PhaseTapChanger pst = transformerFrom.getPhaseTapChanger();
+        if (pst == null) {
+            return;
+        }
+        PhaseTapChangerAdder ptca = transformerTo.newPhaseTapChanger()
+            .setLowTapPosition(pst.getLowTapPosition())
+            .setTapPosition(pst.getTapPosition())
+            .setRegulationValue(pst.getRegulationValue())
+            .setRegulationMode(pst.getRegulationMode());
+        // NB: it is very important to sort the map when copying steps so as to keep the same positions
+        pst.getAllSteps().entrySet()
+            .stream().sorted(Map.Entry.comparingByKey())
+            .map(Map.Entry::getValue).forEach(step ->
+            ptca.beginStep()
+                .setRho(step.getRho())
+                .setAlpha(step.getAlpha())
+                .setR(step.getR())
+                .setX(step.getX())
+                .setG(step.getG())
+                .setB(step.getB())
+                .endStep()
+        );
+        ptca.add();
+    }
+
+    private static void copyProperties(Identifiable<?> identifiableFrom, Identifiable<?> identifiableTo) {
+        identifiableFrom.getPropertyNames().forEach(property ->
+            identifiableTo.setProperty(property, identifiableFrom.getProperty(property))
+        );
+    }
+
+    private LineAdder initializeLineAdderToMove(Line line, String newId) {
+        return network.newLine()
+            .setId(newId)
+            .setR(line.getR())
+            .setX(line.getX())
+            .setG1(line.getG1())
+            .setB1(line.getB1())
+            .setG2(line.getG2())
+            .setB2(line.getB2())
+            .setFictitious(line.isFictitious())
+            .setName(newId);
+    }
+
+    /**
+     * Sets LineAdder's attributes identical to a given side of a branch
+     * WARNING: This only works for bus breaker topology. UCTE import in PowSyBl is always done in bus breaker view.
+     */
+    private static BranchAdder<?> setIdenticalToSide(Branch<?> branch, Branch.Side side, BranchAdder<?> adder) {
+        if (side == Branch.Side.ONE) {
+            return adder.setVoltageLevel1(branch.getTerminal1().getVoltageLevel().getId())
+                .setConnectableBus1(branch.getTerminal1().getBusBreakerView().getConnectableBus().getId())
+                .setBus1(branch.getTerminal1().getBusBreakerView().getBus() != null ? branch.getTerminal1().getBusBreakerView().getBus().getId() : null);
+        } else {
+            return adder.setVoltageLevel2(branch.getTerminal2().getVoltageLevel().getId())
+                .setConnectableBus2(branch.getTerminal2().getBusBreakerView().getConnectableBus().getId())
+                .setBus2(branch.getTerminal2().getBusBreakerView().getBus() != null ? branch.getTerminal2().getBusBreakerView().getBus().getId() : null);
+        }
+    }
+
+    private void setBranchAdderProperties(BranchAdder<?> adder, Branch<?> branchToCopy, Branch.Side sideToUpdate, Bus busToUpdate) {
+        if (sideToUpdate == Branch.Side.ONE) {
+            setIdenticalToSide(branchToCopy, Branch.Side.TWO, adder)
+                .setConnectableBus1(busToUpdate.getId())
+                .setBus1(busToUpdate.getId())
+                .setVoltageLevel1(busToUpdate.getVoltageLevel().getId());
+        } else if (sideToUpdate == Branch.Side.TWO) {
+            setIdenticalToSide(branchToCopy, Branch.Side.ONE, adder)
+                .setConnectableBus2(busToUpdate.getId())
+                .setBus2(busToUpdate.getId())
+                .setVoltageLevel2(busToUpdate.getVoltageLevel().getId());
+        }
+    }
+
+    private String replaceSimpleBranchNode(Branch<?> branch, Branch.Side side, String newNodeId) {
+        String from = (side == Branch.Side.ONE) ? newNodeId : branch.getTerminal1().getBusBreakerView().getBus().getId();
+        String to = (side == Branch.Side.ONE) ? branch.getTerminal2().getBusBreakerView().getBus().getId() : newNodeId;
+        if (branch instanceof TwoWindingsTransformer) {
+            // convention is inverted
+            return generateUcteId(to, from, getOrderCode(branch));
+        } else {
+            return generateUcteId(from, to, getOrderCode(branch));
+        }
+    }
+
+    private static String replaceTieLineNode(TieLine tieLine, Branch.Side side, String newNodeId) {
+        String nodeToReplace = getTieLineNodeToReplace(tieLine, side);
+        return tieLine.getId().replace(nodeToReplace, newNodeId);
+    }
+
+    private static String replaceHalfLineNode(TieLine tieLine, Branch.Side side, String newNodeId) {
+        TieLine.HalfLine halfLine = (side == Branch.Side.ONE) ? tieLine.getHalf1() : tieLine.getHalf2();
+        String nodeToReplace = getTieLineNodeToReplace(tieLine, side);
+        return halfLine.getId().replace(nodeToReplace, newNodeId);
+    }
+
+    private static String getTieLineNodeToReplace(TieLine tieLine, Branch.Side side) {
+        TieLine.HalfLine halfLine = (side == Branch.Side.ONE) ? tieLine.getHalf1() : tieLine.getHalf2();
+        String node1 = halfLine.getId().substring(0, 8);
+        String node2 = halfLine.getId().substring(9, 17);
+        return node1.equals(tieLine.getUcteXnodeCode()) ? node2 : node1;
+    }
+
+    private TieLineAdder initializeTieLineAdderToMove(TieLine tieLine, String newId, Branch.Side sideToUpdate, Bus bus) {
+        TieLine.HalfLine half1 = tieLine.getHalf1();
+        TieLine.HalfLine half2 = tieLine.getHalf2();
+        String xnodeCode = tieLine.getUcteXnodeCode();
+        String newHalf1Id = (sideToUpdate == Branch.Side.ONE) ? replaceHalfLineNode(tieLine, Branch.Side.ONE, bus.getId()) : half1.getId();
+        String newHalf2Id = (sideToUpdate == Branch.Side.TWO) ? replaceHalfLineNode(tieLine, Branch.Side.TWO, bus.getId()) : half2.getId();
+        return network.newTieLine()
+            .setId(newId)
+            .newHalfLine1()
+            .setId(newHalf1Id)
+            .setR(half1.getR())
+            .setX(half1.getX())
+            .setB1(half1.getB1())
+            .setB2(half1.getB2())
+            .setG1(half1.getG1())
+            .setG2(half1.getG2())
+            .setFictitious(half1.isFictitious())
+            .add()
+            .newHalfLine2()
+            .setId(newHalf2Id)
+            .setR(half2.getR())
+            .setX(half2.getX())
+            .setB1(half2.getB1())
+            .setB2(half2.getB2())
+            .setG1(half2.getG1())
+            .setG2(half2.getG2())
+            .setFictitious(half2.isFictitious())
+            .add()
+            .setUcteXnodeCode(xnodeCode);
+    }
+
+    private TwoWindingsTransformerAdder initializeTwoWindingsTransformerAdderToMove(TwoWindingsTransformer twoWindingsTransformer, String newId) {
+        return twoWindingsTransformer.getSubstation().orElseThrow(FaraoException::new).newTwoWindingsTransformer()
+            .setEnsureIdUnicity(true)
+            .setId(newId)
+            .setRatedU1(twoWindingsTransformer.getRatedU1())
+            .setRatedU2(twoWindingsTransformer.getRatedU2())
+            .setR(twoWindingsTransformer.getR())
+            .setX(twoWindingsTransformer.getX())
+            .setG(twoWindingsTransformer.getG())
+            .setB(twoWindingsTransformer.getB())
+            .setFictitious(twoWindingsTransformer.isFictitious());
+    }
+
+    private static void copyCurrentLimits(Branch<?> branchFrom, Branch<?> branchTo) {
+        if (branchFrom.getCurrentLimits1() != null) {
+            branchTo.newCurrentLimits1()
+                .setPermanentLimit(branchFrom.getCurrentLimits1().getPermanentLimit())
+                .add();
+        }
+        if (branchFrom.getCurrentLimits2() != null) {
+            branchTo.newCurrentLimits2()
+                .setPermanentLimit(branchFrom.getCurrentLimits2().getPermanentLimit())
+                .add();
+        }
+    }
+
+    /**
+     * Objects moved by the modifier are not removed until they are no longer needed
+     * Call this function when you want to remove these objects
+     */
+    public void commitAllChanges() {
+        try {
+            connectablesToRemove.forEach(Connectable::remove);
+            connectablesToRemove = new HashSet<>();
+        } catch (PowsyblException e) {
+            throw new FaraoException(String.format("Could not apply all changes to network: %s", e.getMessage()));
+        }
+    }
+}
