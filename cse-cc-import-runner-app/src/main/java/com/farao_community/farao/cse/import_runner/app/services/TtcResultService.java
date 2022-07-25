220111a34a23c9c63e932ca8f2241db113fe6693
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2022-07-25 16:53:36 +0200

    reintegrate busbar pre-treatment when importing crac from xml

--- cse-cc-import-runner-app/src/main/java/com/farao_community/farao/cse/import_runner/app/services/TtcResultService.java
+++ cse-cc-import-runner-app/src/main/java/com/farao_community/farao/cse/import_runner/app/services/TtcResultService.java
@@ -13,13 +13,17 @@ import com.farao_community.farao.cse.data.ttc_res.TtcResult;
 import com.farao_community.farao.cse.data.xnode.XNodeReader;
 import com.farao_community.farao.cse.data.xsd.ttc_res.Timestamp;
 import com.farao_community.farao.cse.import_runner.app.util.FileUtil;
+import com.farao_community.farao.cse.network_processing.busbar_change.BusBarChangeProcessor;
 import com.farao_community.farao.data.crac_api.Crac;
 import com.farao_community.farao.cse.runner.api.resource.CseRequest;
 import com.farao_community.farao.cse.import_runner.app.CseData;
 import com.farao_community.farao.cse.import_runner.app.configurations.XNodesConfiguration;
 import com.farao_community.farao.data.crac_creation.creator.api.CracCreators;
+import com.farao_community.farao.data.crac_creation.creator.api.parameters.CracCreationParameters;
 import com.farao_community.farao.data.crac_creation.creator.cse.CseCrac;
 import com.farao_community.farao.data.crac_creation.creator.cse.CseCracCreationContext;
+import com.farao_community.farao.data.crac_creation.creator.cse.parameters.BusBarChangeSwitches;
+import com.farao_community.farao.data.crac_creation.creator.cse.parameters.CseCracCreationParameters;
 import com.farao_community.farao.data.rao_result_api.RaoResult;
 import com.farao_community.farao.dichotomy.api.results.LimitingCause;
 import com.farao_community.farao.rao_runner.api.resource.RaoResponse;
@@ -27,6 +31,7 @@ import com.powsybl.iidm.network.Network;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
+import java.util.Set;
 
 /**
  * @author Joris Mancini {@literal <joris.mancini at rte-france.com>}
@@ -72,11 +77,21 @@ public class TtcResultService {
         // otherwise the researches in TTC result creation would fail...
 
         CseCrac nativeCseCrac = fileImporter.importCseCrac(cseRequest.getMergedCracUrl());
-        CseCracCreationContext cseCracCreationContext = (CseCracCreationContext) CracCreators.createCrac(nativeCseCrac, networkAfterDichotomy, cseRequest.getTargetProcessDateTime());
+        CracCreationParameters cracCreationParameters = integrateBusBarPretreatment(networkAfterDichotomy, nativeCseCrac);
+        CseCracCreationContext cseCracCreationContext = (CseCracCreationContext) CracCreators.createCrac(nativeCseCrac, networkAfterDichotomy, cseRequest.getTargetProcessDateTime(), cracCreationParameters);
         Crac cracResult = fileImporter.importCracFromJson(highestSecureStepRaoResponse.getCracFileUrl());
         RaoResult raoResult = fileImporter.importRaoResult(highestSecureStepRaoResponse.getRaoResultFileUrl(), cracResult);
         Timestamp timestamp = TtcResult.generate(ttcFiles, processData, new CracResultsHelper(cseCracCreationContext, raoResult, XNodeReader.getXNodes(xNodesConfiguration.getxNodesFilePath())));
         return fileExporter.saveTtcResult(timestamp, cseRequest.getTargetProcessDateTime(), cseRequest.getProcessType());
+    }
+
+    private CracCreationParameters integrateBusBarPretreatment(Network networkAfterDichotomy, CseCrac nativeCseCrac) {
+        Set<BusBarChangeSwitches> busBarChangeSwitchesSet = BusBarChangeProcessor.process(networkAfterDichotomy, nativeCseCrac);
+        CseCracCreationParameters cseCracCreationParameters = new CseCracCreationParameters();
+        cseCracCreationParameters.setBusBarChangeSwitchesSet(busBarChangeSwitchesSet);
+        CracCreationParameters cracCreationParameters = CracCreationParameters.load();
+        cracCreationParameters.addExtension(CseCracCreationParameters.class, cseCracCreationParameters);
+        return cracCreationParameters;
     }
 
     private static TtcResult.TtcFiles createTtcFiles(CseRequest cseRequest, String baseCaseFileUrl, String finalCgmUrl) {
