9eb8ce2a45b5941b553a30640f9b95b085bac3ac
SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>
2023-03-01 11:58:05 +0100

    add unit test on cse runner class

--- cse-cc-import-runner-app/src/test/java/com/farao_community/farao/cse/import_runner/app/services/CseRunnerTest.java
+++ cse-cc-import-runner-app/src/test/java/com/farao_community/farao/cse/import_runner/app/services/CseRunnerTest.java
@@ -8,13 +8,29 @@
 package com.farao_community.farao.cse.import_runner.app.services;
 
 import com.farao_community.farao.cse.import_runner.app.CseData;
+import com.farao_community.farao.cse.import_runner.app.dichotomy.DichotomyRaoResponse;
+import com.farao_community.farao.cse.import_runner.app.dichotomy.MultipleDichotomyResult;
+import com.farao_community.farao.cse.import_runner.app.dichotomy.MultipleDichotomyRunner;
+import com.farao_community.farao.cse.runner.api.resource.CseRequest;
+import com.farao_community.farao.cse.runner.api.resource.CseResponse;
+import com.farao_community.farao.cse.runner.api.resource.ProcessType;
 import com.farao_community.farao.data.crac_api.Crac;
+import com.farao_community.farao.dichotomy.api.results.DichotomyResult;
 import com.powsybl.iidm.network.Network;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.time.OffsetDateTime;
 import java.util.Map;
 import java.util.Objects;
@@ -22,6 +38,7 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 /**
  * @author Joris Mancini {@literal <joris.mancini at rte-france.com>}
@@ -31,6 +48,9 @@ class CseRunnerTest {
 
     @Autowired
     private CseRunner cseRunner;
+
+    @MockBean
+    private MultipleDichotomyRunner multipleDichotomyRunner;
 
     @Test
     void testCracImportAndBusbarPreprocess() {
@@ -58,5 +78,62 @@ class CseRunnerTest {
         );
         Mockito.when(cseData.getNtcPerCountry()).thenReturn(ntcPerCountry);
         assertEquals(5610., cseRunner.getInitialIndexValueForD2ccProcess(cseData));
+    }
+
+    @Test
+    void testRun() {
+        CseRequest cseRequest = null;
+        try {
+            cseRequest = new CseRequest(
+                    "ID1",
+                    ProcessType.IDCC,
+                    OffsetDateTime.parse("2021-09-01T20:30Z"),
+                    getClass().getResource("20210901_2230_test_network_pisa_test_both_links_connected_setpoint_and_emulation_ok_for_run.uct").toURI().toURL().toString(),
+                    getClass().getResource("20210901_2230_213_CRAC_CO_CSE1.xml").toURI().toURL().toString(),
+                    getClass().getResource("crac.xml").toURI().toURL().toString(),
+                    getClass().getResource("20210624_2D4_NTC_reductions_CSE1_Adapted_v8_8.xml").toURI().toURL().toString(),
+                    null,
+                    null,
+                    null,
+                    null,
+                    null,
+                    getClass().getResource("vulcanus_01032021_96.xls").toURI().toURL().toString(),
+                    getClass().getResource("2021_2Dp_NTC_annual_CSE1_Adapted_v8_8.xml").toURI().toURL().toString(),
+                    null,
+                    null,
+                    null,
+                    50.0,
+                    0.0,
+                    null,
+                    true
+            );
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            MultipleDichotomyResult<DichotomyRaoResponse> dichotomyResult = mock(MultipleDichotomyResult.class);
+
+            when(multipleDichotomyRunner.runMultipleDichotomy(
+                    any(CseRequest.class),
+                    any(CseData.class),
+                    any(Network.class),
+                    any(Crac.class),
+                    anyDouble(),
+                    any(Map.class)
+            )).thenReturn(dichotomyResult);
+            DichotomyResult<DichotomyRaoResponse> raoResponse = mock(DichotomyResult.class);
+
+            when(dichotomyResult.getBestDichotomyResult()).thenReturn(raoResponse);
+            when(raoResponse.hasValidStep()).thenReturn(false);
+            CseResponse response = cseRunner.run(cseRequest);
+
+            assertNotNull(response);
+            assertTrue(StringUtils.contains(response.getTtcFileUrl(), "IMPORT-ADAPTED"));
+            assertTrue(StringUtils.contains(response.getFinalCgmFileUrl(), "IMPORT-ADAPTED"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
