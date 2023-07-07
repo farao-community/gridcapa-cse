84dc89e25b81dfbb1e5c6da573e1c14a9a65d588
SCHWITZGUEBEL Marc Ext <schwitzguebelmar@rte-france.com>
2023-07-07 10:29:54 +0200

    fix code style

--- cse-cc-import-runner-app/src/main/java/com/farao_community/farao/cse/import_runner/app/services/TtcResultService.java
+++ cse-cc-import-runner-app/src/main/java/com/farao_community/farao/cse/import_runner/app/services/TtcResultService.java
@@ -62,7 +62,7 @@ public class TtcResultService {
         double finalItalianImport = BorderExchanges.computeItalianImport(networkWithPra);
 
         Map<String, Double> ntcsByEic = cseRequest.getProcessType().equals(ProcessType.IDCC) ?
-            cseData.getNtc2().getExchanges():
+            cseData.getNtc2().getExchanges() :
             NetworkShifterUtil.convertMapByCountryToMapByEic(cseData.getNtcPerCountry());
 
         TtcResult.ProcessData processData = new TtcResult.ProcessData(
