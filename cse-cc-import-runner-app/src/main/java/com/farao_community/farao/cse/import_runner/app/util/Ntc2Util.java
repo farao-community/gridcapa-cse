a24cde8e49d41db32efd3460b3cc227f3f609e33
schwitzguebel <37381897+schwitzguebel@users.noreply.github.com>
2025-10-30 10:46:38 +0100

    Handle clock change (#249)
    * handle clock change for summer saving time
    
    * handle clock change for summer saving time, update tests
    
    * handle clock change for summer saving time, update tests, fix sonar issues
    
    * handle clock change for summer saving time, update tests, complete coverage
    
    * refactoring to have precise daylight saving time management
    
    * correct id inside test files
    
    * add final to variable
    
    * post review update
    
    * post review update
    
    * clean up with rabbit review
    
    ---------
    
    Co-authored-by: schwitzguebelmar <marc.schwitzguebel_externe@rte-france.com>

--- cse-cc-import-runner-app/src/main/java/com/farao_community/farao/cse/import_runner/app/util/Ntc2Util.java
+++ cse-cc-import-runner-app/src/main/java/com/farao_community/farao/cse/import_runner/app/util/Ntc2Util.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.farao_community.farao.cse.import_runner.app.util;
 
 import com.farao_community.farao.cse.data.CseDataException;
@@ -6,8 +12,10 @@ import com.farao_community.farao.cse.data.xsd.ntc2.CapacityDocument;
 import com.powsybl.iidm.network.Country;
 import com.powsybl.openrao.commons.EICode;
 import jakarta.xml.bind.JAXBException;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.InputStream;
+import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -24,15 +32,18 @@ public final class Ntc2Util {
     private static final String FRENCH_TAG = "FR";
     private static final String SLOVENIAN_TAG = "SI";
     private static final String IT_AREA_CODE = "10YIT-GRTN-----B";
+    public static final String CET_ZONE_ID = "CET";
 
     private Ntc2Util() {
 
     }
 
-    public static Double getD2ExchangeByOffsetDateTime(InputStream ntc2InputStream, OffsetDateTime targetDateTime) throws JAXBException {
-        Map<Integer, Double> qtyByPositionMap = new HashMap<>();
-        CapacityDocument capacityDocument = DataUtil.unmarshalFromInputStream(ntc2InputStream, CapacityDocument.class);
-        checkTimeInterval(capacityDocument, targetDateTime);
+    public static Double getD2ExchangeByOffsetDateTime(final InputStream ntc2InputStream, final OffsetDateTime targetDateTime) throws JAXBException {
+        final Map<Integer, Double> qtyByPositionMap = new HashMap<>();
+        final CapacityDocument capacityDocument = DataUtil.unmarshalFromInputStream(ntc2InputStream, CapacityDocument.class);
+        final ZonedDateTime targetDateInCETZone = targetDateTime.atZoneSameInstant(ZoneId.of(CET_ZONE_ID));
+        final List<OffsetDateTime> documentInterval = getDocumentInterval(capacityDocument);
+        checkTimeInterval(documentInterval, targetDateTime);
 
         capacityDocument.getCapacityTimeSeries()
                 .stream()
@@ -47,28 +58,49 @@ public final class Ntc2Util {
                     qtyByPositionMap.put(index, interval.getQty().getV().doubleValue());
                 });
         int position;
-        ZonedDateTime targetDateInCETZone = targetDateTime.atZoneSameInstant(ZoneId.of("CET"));
-        if (qtyByPositionMap.size() == 96) {
+
+        final int positionHour = calculateHourPositionFromOffsets(targetDateInCETZone, capacityDocument);
+        final int positionMapSize = qtyByPositionMap.size();
+        if (hasCorrectNbOfQuarterHoursInDay(documentInterval, positionMapSize)) {
             if (targetDateInCETZone.getMinute() % 15 != 0) {
-                throw new CseDataException("Minutes must be a multiple of 15 for 96 intervals");
+                throw new CseDataException("Minutes must be a multiple of 15 for 96 intervals (or 92 or 100 on daylight saving time changes)");
             }
-            position = 1 + (4 * targetDateInCETZone.getHour()) + (targetDateInCETZone.getMinute() / 15);
-        } else if (qtyByPositionMap.size() == 24) {
-            position = targetDateInCETZone.getHour() + 1;
+            position = 1 + (4 * positionHour) + (targetDateInCETZone.getMinute() / 15);
+        } else if (hasCorrectNbOfHoursInDay(documentInterval, positionMapSize)) {
+            position = positionHour + 1;
         } else {
-            throw new CseDataException(String.format("CapacityTimeSeries contains %s intervals which is different to 24 or 96", qtyByPositionMap.size()));
+            throw new CseDataException(String.format("CapacityTimeSeries contains %s intervals which is different to 24 or 96 (or 23/92 or 25/100 on daylight saving time changes)", qtyByPositionMap.size()));
         }
 
         return qtyByPositionMap.get(position);
     }
 
-    private static void checkTimeInterval(CapacityDocument capacityDocument, OffsetDateTime targetDateTime) {
-        List<OffsetDateTime> interval = Stream.of(capacityDocument.getCapacityTimeInterval().getV().split("/"))
+    private static void checkTimeInterval(List<OffsetDateTime> interval, OffsetDateTime targetDateTime) {
+        if (targetDateTime.isBefore(interval.get(0)) || !targetDateTime.isBefore(interval.get(1))) {
+            throw new CseDataException("Target date time is out of bounds for NTC2 archive");
+        }
+    }
+
+    private static @NotNull List<OffsetDateTime> getDocumentInterval(final CapacityDocument capacityDocument) {
+        return Stream.of(capacityDocument.getCapacityTimeInterval().getV().split("/"))
                 .map(OffsetDateTime::parse)
                 .toList();
-        if (targetDateTime.isBefore(interval.get(0)) || targetDateTime.isAfter(interval.get(1)) || targetDateTime.equals(interval.get(1))) {
-            throw new CseDataException("Target date time is out of bound for NTC2 archive");
-        }
+    }
+
+    private static int calculateHourPositionFromOffsets(final ZonedDateTime targetDateTime, final CapacityDocument capacityDocument) {
+        final OffsetDateTime beginDateTime = getDocumentInterval(capacityDocument).getFirst();
+        return (int) Duration.between(beginDateTime, targetDateTime).toHours();
+    }
+
+    private static boolean hasCorrectNbOfQuarterHoursInDay(List<OffsetDateTime> interval, int inputQuarterHoursInDay) {
+        return inputQuarterHoursInDay % 4 == 0 && hasCorrectNbOfHoursInDay(interval, inputQuarterHoursInDay / 4);
+    }
+
+    private static boolean hasCorrectNbOfHoursInDay(List<OffsetDateTime> interval, int inputHoursInDay) {
+        final ZonedDateTime intervalBegin = interval.get(0).atZoneSameInstant(ZoneId.of(CET_ZONE_ID));
+        final ZonedDateTime intervalEnd = interval.get(1).atZoneSameInstant(ZoneId.of(CET_ZONE_ID));
+        final long hoursBetween = Duration.between(intervalBegin, intervalEnd).toHours();
+        return hoursBetween == inputHoursInDay;
     }
 
     public static Optional<String> getAreaCodeFromFilename(String fileName) {
---
+++ cse-cc-import-runner-app/src/test/java/com/farao_community/farao/cse/import_runner/app/util/Ntc2UtilTest.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2025, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.farao_community.farao.cse.import_runner.app.util;
+
+import com.farao_community.farao.cse.data.CseDataException;
+import jakarta.xml.bind.JAXBException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.Optional;
+
+class Ntc2UtilTest {
+
+    @Test
+    void getD2ExchangeByOffsetDateTimeAfter() throws IOException {
+        final OffsetDateTime targetDate = OffsetDateTime.parse("2025-10-27T01:00+01:00");
+        final InputStream inputStream = Objects.requireNonNull(getClass().getResource("NTC2_20251026_2D1_CH-IT1-25-hour-case.xml")).openStream();
+        Assertions.assertThatExceptionOfType(CseDataException.class).isThrownBy(() -> Ntc2Util.getD2ExchangeByOffsetDateTime(inputStream, targetDate))
+                .withMessage("Target date time is out of bounds for NTC2 archive");
+    }
+
+    @Test
+    void getD2ExchangeByOffsetDateTimeBefore() throws IOException {
+        final OffsetDateTime targetDate = OffsetDateTime.parse("2025-10-25T22:00+01:00");
+        final InputStream inputStream = Objects.requireNonNull(getClass().getResource("NTC2_20251026_2D1_CH-IT1-25-hour-case.xml")).openStream();
+        Assertions.assertThatExceptionOfType(CseDataException.class).isThrownBy(() -> Ntc2Util.getD2ExchangeByOffsetDateTime(inputStream, targetDate))
+                .withMessage("Target date time is out of bounds for NTC2 archive");
+    }
+
+    @Test
+    void getD2ExchangeByOffsetDateTimeAfterWinterTimeChange() throws JAXBException, IOException {
+        final OffsetDateTime targetDate = OffsetDateTime.parse("2025-10-26T23:00+01:00");
+        final InputStream inputStream = Objects.requireNonNull(getClass().getResource("NTC2_20251026_2D1_CH-IT1-25-hour-case.xml")).openStream();
+        Assertions.assertThat(Ntc2Util.getD2ExchangeByOffsetDateTime(inputStream, targetDate)).isEqualTo(1789);
+    }
+
+    @Test
+    void getD2ExchangeByOffsetDateTimeBeforeWinterTimeChange() throws JAXBException, IOException {
+        final OffsetDateTime targetDate = OffsetDateTime.parse("2025-10-26T02:00+02:00");
+        final InputStream inputStream = Objects.requireNonNull(getClass().getResource("NTC2_20251026_2D1_CH-IT1-25-hour-case.xml")).openStream();
+        Assertions.assertThat(Ntc2Util.getD2ExchangeByOffsetDateTime(inputStream, targetDate)).isEqualTo(2767);
+    }
+
+    @Test
+    void getD2ExchangeByOffsetDateTimeOnWinterTimeChange() throws JAXBException, IOException {
+        final OffsetDateTime targetDate = OffsetDateTime.parse("2025-10-26T03:00+02:00");
+        final InputStream inputStream = Objects.requireNonNull(getClass().getResource("NTC2_20251026_2D1_CH-IT1-25-hour-case.xml")).openStream();
+        Assertions.assertThat(Ntc2Util.getD2ExchangeByOffsetDateTime(inputStream, targetDate)).isEqualTo(8888);
+    }
+
+    @Test
+    void getD2ExchangeByOffsetDateTimeOnWinterTimeChange2() throws JAXBException, IOException {
+        final OffsetDateTime targetDate = OffsetDateTime.parse("2025-10-26T02:00+01:00");
+        final InputStream inputStream = Objects.requireNonNull(getClass().getResource("NTC2_20251026_2D1_CH-IT1-25-hour-case.xml")).openStream();
+        Assertions.assertThat(Ntc2Util.getD2ExchangeByOffsetDateTime(inputStream, targetDate)).isEqualTo(8888);
+    }
+
+    @Test
+    void getAreaCodeFromFilenameDoesntExist() {
+        Assertions.assertThat(Ntc2Util.getAreaCodeFromFilename("tweet")).isEmpty();
+    }
+
+    @Test
+    void getAreaCodeFromFilename() {
+        Assertions.assertThat(Ntc2Util.getAreaCodeFromFilename("NTC2_20251026_2D1_CH-IT1-25-hour-case.xml")).isEqualTo(Optional.of("10YCH-SWISSGRIDZ"));
+    }
+
+    @Test
+    void getD2ExchangeByOffsetDateTimeAfterSummerTimeChange() throws JAXBException, IOException {
+        final OffsetDateTime targetDate = OffsetDateTime.parse("2025-03-30T23:00+02:00");
+        final InputStream inputStream = Objects.requireNonNull(getClass().getResource("NTC2_20250330_2D1_CH-IT1-23-hour-case.xml")).openStream();
+        Assertions.assertThat(Ntc2Util.getD2ExchangeByOffsetDateTime(inputStream, targetDate)).isEqualTo(1789);
+    }
+
+    @Test
+    void getD2ExchangeByOffsetDateTimeBeforeSummerTimeChange() throws JAXBException, IOException {
+        final OffsetDateTime targetDate = OffsetDateTime.parse("2025-03-30T01:00+01:00");
+        final InputStream inputStream = Objects.requireNonNull(getClass().getResource("NTC2_20250330_2D1_CH-IT1-23-hour-case.xml")).openStream();
+        Assertions.assertThat(Ntc2Util.getD2ExchangeByOffsetDateTime(inputStream, targetDate)).isEqualTo(2222);
+    }
+
+    @Test
+    void getD2ExchangeByOffsetDateTimeOnSummerTimeChange() throws JAXBException, IOException {
+        final OffsetDateTime targetDate = OffsetDateTime.parse("2025-03-30T02:00+01:00");
+        final InputStream inputStream = Objects.requireNonNull(getClass().getResource("NTC2_20250330_2D1_CH-IT1-23-hour-case.xml")).openStream();
+        Assertions.assertThat(Ntc2Util.getD2ExchangeByOffsetDateTime(inputStream, targetDate)).isEqualTo(8888);
+    }
+
+    @Test
+    void getD2ExchangeByOffsetDateTimeOnSummerTimeChange2() throws JAXBException, IOException {
+        final OffsetDateTime targetDate = OffsetDateTime.parse("2025-03-30T03:00+02:00");
+        final InputStream inputStream = Objects.requireNonNull(getClass().getResource("NTC2_20250330_2D1_CH-IT1-23-hour-case.xml")).openStream();
+        Assertions.assertThat(Ntc2Util.getD2ExchangeByOffsetDateTime(inputStream, targetDate)).isEqualTo(8888);
+    }
+
+    @Test
+    void getD2ExchangeByOffsetDateTimeOnSummerTimeChange100() throws JAXBException, IOException {
+        final OffsetDateTime targetDate = OffsetDateTime.parse("2025-10-26T23:30+01:00");
+        final InputStream inputStream = Objects.requireNonNull(getClass().getResource("NTC2_20251026_2D5_CH-IT1-test-100.xml")).openStream();
+        Assertions.assertThat(Ntc2Util.getD2ExchangeByOffsetDateTime(inputStream, targetDate)).isEqualTo(4321);
+    }
+
+    @Test
+    void getD2ExchangeByOffsetDateTimeOnSummerTimeChange94() throws JAXBException, IOException {
+        final OffsetDateTime targetDate = OffsetDateTime.parse("2025-03-30T23:30+02:00");
+        final InputStream inputStream = Objects.requireNonNull(getClass().getResource("NTC2_20250330_2D5_CH-IT1-test-92.xml")).openStream();
+        Assertions.assertThat(Ntc2Util.getD2ExchangeByOffsetDateTime(inputStream, targetDate)).isEqualTo(4321);
+    }
+
+    @Test
+    void getD2ExchangeByOffsetDateTimeWrongintervals() throws IOException {
+        final OffsetDateTime targetDate = OffsetDateTime.parse("2021-09-02T03:00+02:00");
+        final InputStream inputStream = Objects.requireNonNull(getClass().getResource("NTC2_20210901_2D1_CH-IT1-bad-case.xml")).openStream();
+        Assertions.assertThatExceptionOfType(CseDataException.class).isThrownBy(() -> Ntc2Util.getD2ExchangeByOffsetDateTime(inputStream, targetDate))
+                .withMessage("CapacityTimeSeries contains 21 intervals which is different to 24 or 96 (or 23/92 or 25/100 on daylight saving time changes)");
+    }
+
+    @Test
+    void getD2ExchangeByOffsetDateTimeWrongTargetDate() throws IOException {
+        final OffsetDateTime targetDate = OffsetDateTime.parse("2025-10-26T23:11+02:00");
+        final InputStream inputStream = Objects.requireNonNull(getClass().getResource("NTC2_20251026_2D5_CH-IT1-test-100.xml")).openStream();
+        Assertions.assertThatExceptionOfType(CseDataException.class).isThrownBy(() -> Ntc2Util.getD2ExchangeByOffsetDateTime(inputStream, targetDate))
+                .withMessage("Minutes must be a multiple of 15 for 96 intervals (or 92 or 100 on daylight saving time changes)");
+    }
+}
